dec88b3ea6beaf540234071e986c551ce371c704
SUBHANKAR DHAR <vdhar29@gmail.com>
2025-12-12 16:50:09 +0530

    arrow

--- src/components/InventoryHub.jsx
+++ src/components/InventoryHub.jsx
@@ -32,6 +32,27 @@ const InventoryHub = () => {
 
             <div className="hub-content">
                 <div className="cycle-container">
+                    <svg className="cycle-svg" viewBox="0 0 900 600">
+                        <defs>
+                            <marker id="arrowhead" markerWidth="6" markerHeight="4" refX="5" refY="2" orient="auto">
+                                <polygon points="0 0, 6 2, 0 4" fill="var(--primary-orange)" />
+                            </marker>
+                        </defs>
+                        {/* GRN -> DSU */}
+                        <path d="M 260 120 Q 300 60 350 60" markerEnd="url(#arrowhead)" />
+
+                        {/* DSU -> SPP */}
+                        <path d="M 600 70 Q 630 70 640 120" markerEnd="url(#arrowhead)" />
+
+                        {/* SPP -> SPU */}
+                        <path d="M 750 220 Q 780 280 790 290" markerEnd="url(#arrowhead)" />
+
+                        {/* SPU -> FGI */}
+                        <path d="M 550 320 Q 450 320 390 290" markerEnd="url(#arrowhead)" />
+
+                        {/* FGI -> GDN */}
+                        <path d="M 240 280 Q 200 400 300 500" markerEnd="url(#arrowhead)" />
+                    </svg>
 
                     {/* 1. Goods Received Note */}
                     <motion.div
@@ -42,12 +63,6 @@ const InventoryHub = () => {
                         Goods Received Note
                     </motion.div>
 
-                    {/* Arrow 1: GRN -> DSU */}
-                    <svg className="arrow-path" style={{ top: '8%', left: '25%', width: '150px', height: '80px' }} viewBox="0 0 150 80">
-                        <path d="M10,60 Q60,10 130,30" />
-                        <polygon points="130,30 120,25 125,38" transform="rotate(-15 130 30)" />
-                    </svg>
-
                     {/* 2. Daily Store Update */}
                     <motion.div
                         className="cycle-item item-dsu"
@@ -55,12 +70,6 @@ const InventoryHub = () => {
                     >
                         Daily Store Update
                     </motion.div>
-
-                    {/* Arrow 2: DSU -> SPP */}
-                    <svg className="arrow-path" style={{ top: '8%', right: '28%', width: '150px', height: '80px' }} viewBox="0 0 150 80">
-                        <path d="M10,30 Q80,10 130,50" />
-                        <polygon points="130,50 120,40 132,40" transform="rotate(35 130 50)" />
-                    </svg>
 
                     {/* 3. Spare Parts Purchase */}
                     <motion.div
@@ -70,12 +79,6 @@ const InventoryHub = () => {
                         Spare Parts Purchase
                     </motion.div>
 
-                    {/* Arrow 3: SPP -> SPU */}
-                    <svg className="arrow-path" style={{ top: '32%', right: '12%', width: '120px', height: '120px' }} viewBox="0 0 120 120">
-                        <path d="M100,10 Q110,60 40,100" />
-                        <polygon points="40,100 50,95 45,90" transform="rotate(145 40 100)" />
-                    </svg>
-
                     {/* 4. Spare Parts Update */}
                     <motion.div
                         className="cycle-item item-spu"
@@ -83,12 +86,6 @@ const InventoryHub = () => {
                     >
                         Spare Parts Update
                     </motion.div>
-
-                    {/* Arrow 4: SPU -> FGI */}
-                    <svg className="arrow-path" style={{ top: '55%', left: '42%', width: '220px', height: '80px' }} viewBox="0 0 220 80">
-                        <path d="M210,10 Q110,90 20,50" />
-                        <polygon points="20,50 30,55 32,45" transform="rotate(25 20 50)" />
-                    </svg>
 
                     {/* 5. Finished Goods Inventory */}
                     <motion.div
@@ -98,12 +95,6 @@ const InventoryHub = () => {
                         Finished Goods Inventory
                     </motion.div>
 
-                    {/* Arrow 5: FGI -> GDN */}
-                    <svg className="arrow-path" style={{ top: '58%', left: '18%', width: '120px', height: '160px' }} viewBox="0 0 120 160">
-                        <path d="M40,10 Q10,80 90,140" />
-                        <polygon points="90,140 80,135 85,130" transform="rotate(40 90 140)" />
-                    </svg>
-
                     {/* 6. Goods Dispatched Note */}
                     <motion.div
                         className="cycle-item item-gdn"
@@ -111,7 +102,6 @@ const InventoryHub = () => {
                     >
                         Goods Dispatched Note
                     </motion.div>
-
                 </div>
             </div>
         </div>
